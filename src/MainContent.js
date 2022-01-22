12f7984565978e8e032f881ecc1172447436199d
Joshua Holtsclaw <jildsmiles@yahoo.com>
2022-01-22 15:27:06 -0500

    added customer state and conditional rendering for phone #

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,11 +1,57 @@
 import React,{ Component } from "react";
-import NavBar from "./NavBar";
 
 
 class MainContent extends Component{
+
+    state = {
+                pageTitle:"Customers",
+                customersCount: 5,
+                customers:[
+                    {id:1,name:"Josh",phone:null,address:{city:"Columbia"}},
+                    {id:2,name:"Clara",phone:"123-123-123",address:{city:"Columbia"}},
+                    {id:3,name:"Finn",phone:"123-123-123",address:{city:"Columbia"}},
+                    {id:4,name:"Emma",phone:null,address:{city:"Columbia"}},
+                ]
+            };
+    
+    // Refresh button onclick
+    onRefreshClick = () => {
+        this.setState({
+            customersCount:"7"
+        })
+    }
+
     render(){
         return(
-            <h1>hello</h1>
+            <div>
+                <h4 className="border-bottom m-1 p-1">{this.state.pageTitle}
+                <span className=" badge bg-secondary m-2">{this.state.customersCount}</span>
+                <button className="btn btn-info" onClick={this.onRefreshClick}>Refresh</button>
+                </h4>
+
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Customer Name</th>
+                            <th>Phone</th>
+                            <th>City</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.customers.map((cust) => {
+                            return(
+                                <tr key={cust.id}>
+                                    <td>{cust.id}</td>
+                                    <td>{cust.name}</td>
+                                    <td>{cust.phone ? (cust.phone) : (<div className="bg-warning text-center p-2">No Phone</div>) }</td>
+                                    <td>{cust.address.city}</td>
+                                </tr>
+                            ) 
+                        })}
+                    </tbody>
+                </table>
+            </div>
         ) 
     }
 }
